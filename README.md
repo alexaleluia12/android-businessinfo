407649123afc71a99b29a2b0faaea7b6931302ae
alexaleluia12 <alexdiasaleluia@gmail.com>
2019-02-23 20:01:19 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,16 @@
+```txt
+
+Udacity - Android Basic: User Interface
+
+Project Business Info
+
+minSdkVersion=16
+
+What I am learn:
+    - a drawable can be added direct to textView (no LinearLayout)
+    - android studio can extract style from xml element
+    - manipulate SVG image
+    - if a drawable does not show right color add the same color to fill and tint
+```
+
+![app-print](img/app.jpeg)
---
+++ app/src/main/java/com/batenteteclonogia/businessinfo/MainActivity.java
@@ -0,0 +1,18 @@
+package com.batenteteclonogia.businessinfo;
+
+import android.graphics.PorterDuff;
+import android.graphics.PorterDuffColorFilter;
+import android.graphics.drawable.Drawable;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
